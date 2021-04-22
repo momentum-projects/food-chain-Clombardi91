737c1b54091990583a7c750f7ebf332448a3791a
Clombardi91 <chl9195@icloud.com>
2021-04-22 14:08:13 -0400

    lyrics.py

---
+++ lyrics.py
@@ -0,0 +1,28 @@
+# make a function for opening sentance
+# new animal is introduced with each series.
+# each series has both unique phrases and repeating phrases.
+# make a function that calls repeated phrases with each series.
+
+animals = [
+    ('fly', 'I dont know she swallowed a fly, perhaps she will die.'),
+    ('spider', 'It wiggled and jiggled and tickled inside her.'),
+    ('bird', 'How absurd, to swallow a bird.'),
+    ('cat', 'Imagine that, she swallowed a cat.'),
+    ('dog', 'What a hog, to swallow a dog.'),
+    ('goat', 'She just opened her throat and swallowed a goat.'),
+    ('cow', 'I dont know how she swallowed a cow.'),
+    ('horse', 'Shes dead of course.')]
+    
+    #intro = "There was an old lady who swallowed a"
+
+def intro(line_1):
+    line_1 = "There was an old lady who swallowed a"
+    
+while line_1 +animal+
+
+def play(animal):
+    for animal in animals:
+        print(f'{animal}')
+        
+
+play(animals)
